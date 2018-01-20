cc6e21f967428071c543dad0229164a8a2e07524
Nevaermorr <pi.wiacek@gmail.com>
2018-01-20 20:23:31 +0100

    Cast trivial components

--- Goblin Commander/Assets/GameScripts/Features/View/Components/GameObjectComponent.cs
+++ Goblin Commander/Assets/GameScripts/Features/View/Components/GameObjectComponent.cs
@@ -5,4 +5,9 @@ using UnityEngine;
 public class GameObjectComponent : IComponent
 {
 	public GameObject Value;
+
+	public static implicit operator GameObject(GameObjectComponent gameObjectComponent)
+	{
+		return gameObjectComponent.Value;
+	}
 }
--- Goblin Commander/Assets/GameScripts/Features/View/Components/SpriteComponent.cs
+++ Goblin Commander/Assets/GameScripts/Features/View/Components/SpriteComponent.cs
@@ -4,4 +4,9 @@ using Entitas;
 public class SpriteComponent : IComponent
 {
 	public Sprite Value;
+
+	public static implicit operator Sprite(SpriteComponent spriteComponent)
+	{
+		return spriteComponent.Value;
+	}
 }
